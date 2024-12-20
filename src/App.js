79dbc8fd2ffdb1ad5ac832cbb9857e065603763c
KEFFA <muthurikeffa254@gmail.com>
2024-12-20 11:49:35 +0300

    Responsive fixing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import './mediaqueries.css'
+import './mobile.css'
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Aboutme from './Components/Aboutme';
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,39 +10,39 @@ return(
             <div className="home-content">
                     <div className="cards-container">
                         <div className="card-1">
-                            <img className="dev_me_img" src={dev_me} alt="Developer"/>
+                            <img className="dev_me_img" src={dev_me} alt="Site owner"/>
                         </div>
                         <div className="card-2"> 
                             <h4>Hi, I'm</h4>
                             <h2>Keffa Muthuri</h2>
                             <h3>Frontend Developer</h3>
                             <div className="twin-btn">
-                            <button className="btn-D">Download cv</button>
-                            <button className="btn-C">Contact info</button>
+                                <button className="btn-D">Download cv</button>
+                                <button className="btn-C">Contact info</button>
+                            </div>
                         </div>
                         <div className="icons-container">
                             <ul>
                                 <li>                    
                                     <a href="https://github.com/efpha" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                                    <FontAwesomeIcon className="social_media_icon" icon={faGithub} size="2x" />
                                     <p>Github</p>
                                     </a>
                                 </li>
                                 <li>                    
                                     <a href="https://www.linkedin.com/in/syncd3v/" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                                    <FontAwesomeIcon className="social_media_icon" icon={faLinkedin} size="2x" />
                                     <p>linkedin</p>
                                     </a>
                                 </li>
                                 <li>                    
                                     <a href="https://x.com/ef_pha" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faXTwitter} size="2x"/>
+                                    <FontAwesomeIcon className="social_media_icon" icon={faXTwitter} size="2x"/>
                                     <p>Twitter/X</p>
                                     </a>
                                 </li>
                             </ul>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
