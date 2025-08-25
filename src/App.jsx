8da3bc95f57c3bed5423afa6cd9ee837f10f381e
KEFFA <muthurikeffa254@gmail.com>
2025-08-25 21:21:25 +0300

    app.js to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './App.css';
+import './mobile.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Aboutme from './Components/Aboutme';
+import Experience from './Components/Experience';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div id="home">
+        <Home />
+      </div>
+      <div id="experience">
+        <Experience />
+      </div>
+      <div id="aboutme">
+        <Aboutme />
+      </div>
+    </div>
+  );
+}
+
+export default App;
