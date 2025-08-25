b5a7a0a55d1f5eb9f119082734bfe917033cc7b3
KEFFA <muthurikeffa254@gmail.com>
2025-08-25 21:20:17 +0300

    Project.jsx Initialization

---
+++ src/Components/project.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Project(){
+    return(
+        <div className="project-container">
+            <h2>Projects</h2>
+            <p>Coming soon...</p>
+        </div>
+    )
+}
